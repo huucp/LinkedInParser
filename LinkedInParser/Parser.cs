394f4ca63059f1de3f18345ff1b48cea32cfa1ab
HuuCP <phamchinhhuu@gmail.com>
2013-07-25 22:48:13 +0700

    save to server

--- LinkedInParser/Parser.cs
+++ LinkedInParser/Parser.cs
@@ -8,19 +8,108 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Net;
+using NLog;
+using MyConnect;
+using MyConnect.SQLServer;
+using MyUtility;
+using Microsoft.Vbe.Interop;
 
 namespace LinkedInParser
 {
     public partial class Parser : Form
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+
         public Parser()
         {
-            InitializeComponent();
 
-            //string content = File.ReadAllText("trung kien thai.html");
-            string content = File.ReadAllText("luu vinh.html");
-            var parserCore = new ParserCore(content);
-            parserCore.Process();
+
+
+            //string content = File.ReadAllText("luu vinh.html");
+            //var parserCore = new ParserCore(content);
+            //parserCore.Process();
+            MainProcess("http://vn.linkedin.com/pub/trung-kien-thai/66/b1/946", 10);
+        }
+        const string scn = "SQLConnectionName";
+        MyGetData mGet = new MyGetData(scn);
+        MyExecuteData mExe = new MyExecuteData(scn);
+        private void MainProcess(string first, int limit)
+        {
+
+            int count = 0;
+            string link = first;
+            while (count < limit)
+            {
+                string content = GetWebContent(link);
+                var parserCore = new ParserCore(content);
+                var profile = parserCore.Process();
+                
+
+                if (!IsExisted(link))
+                {
+                    WriteToDB(profile,link);
+                }
+                link = profile.NextProfile;
+                count++;
+            }
+        }
+
+        private bool IsExisted(string link)
+        {
+            link = link.Replace("'", "\"");
+            string temp = "select * from UserDB where Link='" + link + "'";
+            var tbl = mGet.GetDataTable_Query("select * from UserDB where Link='" + link + "'");
+            if (tbl.Rows.Count == 0) return false;
+            return true;
+        }
+
+        private void WriteToDB(LinkedInProfile profile,string link)
+        {
+            string username = profile.Username.Replace("'", "\"");
+            string position = profile.Position.Replace("'", "\"");
+            string summary = profile.Summary.Replace("'", "\"");
+            string exp = profile.Experience.Replace("'", "\"");
+            string language = string.Empty;
+            foreach (var l in profile.Language)
+            {
+                language += ";" + l.Replace("'", "\"");
+            }
+            string skill = string.Empty;
+            foreach (var s in profile.SkillAndExpertise)
+            {
+                skill = ";" + s.Replace("'", "\"");
+            }
+            link = link.Replace("'", "\"");
+            mExe.ExecQuery("INSERT INTO UserDB (Username,Position,Description,Experience,Language,Skill,Link) " +"VALUES" +" (" +
+                           "'" + username + "'," +
+                           "'" + position + "'," +
+                           "'" + summary + "'," +
+                           "'" + exp + "'," +
+                           "'" + language + "'," +
+                           "'" + skill + "'," +
+                           "'" + link + "'" +
+                           ")");
+        }
+
+        private string GetWebContent(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "GET";
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            if (stream != null)
+            {
+                var reader = new StreamReader(stream);
+                string content = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+                return content;
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
     }
 }
--- LinkedInParser/ParserCore.cs
+++ LinkedInParser/ParserCore.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
+using NLog;
 
 namespace LinkedInParser
 {
@@ -16,40 +17,138 @@ namespace LinkedInParser
             Content = htmlContent;
         }
 
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         // <span class="full-name">: tag for user full name
         // <p class="headline-title title" style="display:block">: tag for position
         // <p class=" description summary">: tag for description
         // <div class="postitle">: tag for begin experience
         // <li class="competency language">: tag for language
         // <li class="competency show-bean  ">: tag for skill
+        // <li class="with-photo">: tag for link profile
         public LinkedInProfile Process()
         {
             var profile = new LinkedInProfile();
-            
-            // Get username            
+            logger.Info("Start parse HTML");
+
+            // Get username       
+            logger.Info("Get username");
             string[] usernameSplitWord = Regex.Split(Content, "<span class=\"full-name\">");
             profile.Username = ExtractUsername(usernameSplitWord[1]);
+            logger.Info(profile.Username);
 
-            // Get position            
+            // Get position 
+            logger.Info("Get position");
             string[] postionSplitWord = Regex.Split(Content, "<p class=\"headline-title title\" style=\"display:block\">");
             profile.Position = ExtractPosition(postionSplitWord[1]);
-            
+            logger.Info(profile.Position);
+
+            // Get summary
+            logger.Info("Get summary");
+            string[] summarySplitWord = Regex.Split(Content, "<p class=\" description summary\">");
+            profile.Summary = ExtractSummary(summarySplitWord[1]);
+
+            // Get experience
+            logger.Info("Get experience");
+            string[] expSplitWord = Regex.Split(Content, "<div class=\"postitle\">");
+            profile.Experience = ExtractExperience(expSplitWord[1]);
+            logger.Info(profile.Experience);
+
+            // Get language
+            logger.Info("Get language");
+            string[] langSplitWord = Regex.Split(Content, "<li class=\"competency language\">");
+            for (int i = 1; i < langSplitWord.Count(); i++)
+            {
+                string lang = ExtractLanguage(langSplitWord[i]);
+                profile.Language.Add(lang);
+                logger.Info(lang);
+            }
+
+            // Get skill and expertise
+            logger.Info("Get skill and expertise");
+            string[] skillSplitWord = Regex.Split(Content, "<li class=\"competency show-bean  \">");
+            for (int i = 1; i < skillSplitWord.Count(); i++)
+            {
+                string skill = ExtractSkillAndExpertise(skillSplitWord[i]);
+                profile.SkillAndExpertise.Add(skill);
+                logger.Info(skill);
+            }
+
+            // Get next profile link
+            logger.Info("Get next profile");
+            string[] nextSplitWord = Regex.Split(Content, "<li class=\"with-photo\">");
+            profile.NextProfile = ExtractNextProfile(nextSplitWord[1]);
+            logger.Info(profile.NextProfile);
+
+            logger.Info("End parse HTML");
             return profile;
         }
+
+        private string ExtractNextProfile(string content)
+        {
+            string[] s1= Regex.Split(content,"<a href=\"");
+            string url = Regex.Split(s1[1], "\">")[0].Trim();
+            return url;
+        }
+
+        private string ExtractSkillAndExpertise(string content)
+        {
+            string[] s1 = Regex.Split(content, "</a>");
+            string skill = Regex.Split(s1[0], ">")[2];
+            return WebUtility.HtmlDecode(skill).Trim();
+        }
+
+        private string ExtractLanguage(string content)
+        {
+            var languageSplit = Regex.Split(content, "<h3>");
+            var lang = Regex.Split(languageSplit[1], "</h3>")[0].Trim();
+            return WebUtility.HtmlDecode(lang).Trim();
+        }
+
         private string ExtractUsername(string content)
         {
             string[] words = Regex.Split(content, "</span> <span class=\"family-name\">");
             string[] givenName = Regex.Split(words[0], "<span class=\"given-name\">");
             string[] familyName = Regex.Split(words[1], "</span></span>");
-            return WebUtility.HtmlDecode(string.Format("{0} {1}", givenName[1], familyName[0]));
+            return WebUtility.HtmlDecode(string.Format("{0} {1}", givenName[1], familyName[0])).Trim();
         }
         private string ExtractPosition(string content)
         {
             string temp = WebUtility.HtmlDecode(Regex.Split(content, "</p>")[0]);
-            string[] parts = temp.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            string[] parts = temp.Split(new string[] { "\n" }, StringSplitOptions.None);
             return parts[3].Trim();
         }
-        
+
+        private string ExtractSummary(string content)
+        {
+            string[] description = Regex.Split(content, "</p>");
+            string s = description[0].Replace("<br>", "");
+            return WebUtility.HtmlDecode(s).Trim();
+        }
+
+        private string ExtractExperience(string content)
+        {
+            string[] titleSplitWord = Regex.Split(content, "<span class=\"title\">");
+            string title = Regex.Split(titleSplitWord[1], "</span>")[0].Trim();
+
+            string[] orgSplitWord = Regex.Split(content, "<span class=\"org summary\">");
+            string org = Regex.Split(orgSplitWord[1], "</span>")[0].Trim();
+
+            string[] orgDetailSplitWord = Regex.Split(content, "<p class=\"orgstats organization-details current-position\">");
+            string orgDetail = string.Empty;
+            if (orgDetailSplitWord.Count() > 1)
+            {
+                orgDetail = Regex.Split(orgDetailSplitWord[1], "</p>")[0].Trim();
+            }
+
+            string[] periodSplitWord = Regex.Split(content, "<p class=\"period\">");
+            string[] timeSplitWord = Regex.Split(periodSplitWord[1], "</abbr>");
+            string firstTime = Regex.Split(timeSplitWord[0], ">")[1].Trim();
+            string secondTime = Regex.Split(timeSplitWord[1], ">")[1].Trim();
+            string duration = Regex.Split(periodSplitWord[1], "</span>")[1].Trim();
+
+            return WebUtility.HtmlDecode(title + " " + org + " " + orgDetail + " " + firstTime + "-" + secondTime + " " + duration);
+        }
     }
 
     public class LinkedInProfile
@@ -58,8 +157,8 @@ namespace LinkedInParser
         public string Position { get; set; }
         public string Summary { get; set; }
         public string Experience { get; set; }
-        public List<string> Language { get; set; }
-        public List<string> SkillAndExpertise { get; set; }
+        public List<string> Language = new List<string>();
+        public List<string> SkillAndExpertise = new List<string>();
         public string NextProfile { get; set; }
     }
 }
